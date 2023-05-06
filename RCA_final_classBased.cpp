4c3c5fc9e47180e2dd655d8a84e82020470b7c99
Sukruth <sukruth.akrao@gmail.com>
2023-05-05 23:57:31 -0700

    RCA in OO C++

--- RCA_final_classBased.cpp
+++ RCA_final_classBased.cpp
@@ -19,57 +19,52 @@ class printArray {
 
 };
 
-
-void printarray(short array1[32]) {
-    //cout << "the array is : ";
-    for(int i = 31; i >= 0; i--){
-        cout << array1[i] << " "; 
-    }
-    cout << "\n";
-
-}
-
-short *IntToBinaryArray(unsigned int e) {
-    short *arr = (short*) malloc(32 * sizeof(short));
-    //int array[32];
-    for (int i = 0; i < 32; i++) {
-        arr[i] = e % 2;
-        e = e / 2;
-    }
-    //cout << arr;
-    return arr;
-}
-
-struct RippleCarryAdder {
-	int delayOfAdder;
-	short* sum;
-	int carryOut;
+class IntToBin {
+	public:
+		short *IntToBinaryArray(unsigned int e) {
+			short *arr = (short*) malloc(32 * sizeof(short));
+			for(int i = 0; i < 32; i++) {
+				arr[i] = e % 2;
+				e = e / 2;
+			}
+			return arr;
+		}
 };
-RippleCarryAdder rca_1(short a_arr[32], short b_arr[32]) {
+
+class RippleCarryAdder_32 {
+
+	public:
+		int delayOfAdder;
+		short* sum;
+		int carryOut;
+		
+};
+RippleCarryAdder_32 rca(short a_arr[32], short b_arr[32]) {
+
+	RippleCarryAdder_32 rca_1;
 
 	short *sum = (short*) malloc(32 * sizeof(short));
 	int c = 0;
-    	int D = 0;
-    	int Dmax = 0;
+	int D = 0;
+	int Dmax = 0;
 
-    	for (int i = 0; i < 32; i++) {
-
-        	sum[i] = a_arr[i] ^ b_arr[i] ^ c;
-    	 	if (c == 0) {
-            		if ((a_arr[i] == 0) && (b_arr[i] == 0)) {
-                		Dmax = std::max(Dmax, D);
-                		D = 0;
-            		}
-            		else if ((a_arr[i] ^ b_arr[i]) == 1) {
-                		D = 2;
-                		Dmax = std::max(Dmax, D);
-                		D = 0;
-            		}
-            		else if ((a_arr[i] & b_arr[i]) == 1) {
-                		D = 2;
-                		Dmax = std::max(Dmax, D);
-            		}
-        	}
+	for (int i = 0; i < 32; i++) {
+		sum[i] = a_arr[i] ^ b_arr[i] ^ c;
+		if (c == 0) {
+			if ((a_arr[i] == 0) && (b_arr[i] == 0)) {
+				Dmax = std::max(Dmax, D);
+				D = 0;
+			}
+			else if ((a_arr[i] ^ b_arr[i]) == 1) {
+				D = 2;
+				Dmax = std::max(Dmax, D);
+				D = 0;
+			}
+			else if ((a_arr[i] & b_arr[i]) == 1) {
+				D = 2;
+				Dmax = std::max(Dmax, D);
+			}
+		}
 		if (c == 1) {
 			if ((a_arr[i] == 0) && (b_arr[i] == 0)) {
 				D = D + 2;
@@ -89,17 +84,12 @@ RippleCarryAdder rca_1(short a_arr[32], short b_arr[32]) {
 		c = ((a_arr[i] & b_arr[i]) | c & (a_arr[i] ^ b_arr[i]));
 	}
 
-	RippleCarryAdder rca1;
-	rca1.delayOfAdder = Dmax;
-	rca1.sum = sum;
-	rca1.carryOut = c;
+	rca_1.delayOfAdder = Dmax;
+	rca_1.sum = sum;
+	rca_1.carryOut = c;
 
-	return rca1;
+	return rca_1;
 }
-
-
-//int *rca(int a_arr[32], int b_arr[32]){
-   
 
 int main() {
 
@@ -108,21 +98,23 @@ int main() {
 	cin >> x;
 	cin >> y;
 
+	IntToBin IntToBinObj;
+	printArray printArrayObj; //object of printArray
+
 	short *a;
-	a = test1(x);
+	a = IntToBinObj.IntToBinaryArray(x);
 	cout << " A : ";
-	printarray(a);
+	printArrayObj.displayArray(a);
 
 	short *b;
-	b = test1(y);
-
+	b = IntToBinObj.IntToBinaryArray(y);
 	cout << " B : ";
-	printarray(b);
+	printArrayObj.displayArray(b);
 
-	RippleCarryAdder RCA = rca_1(a,b);
 
+	RippleCarryAdder_32 RCA = rca(a,b);
 	cout << "Sum: ";
-	printarray(RCA.sum);
+	printArrayObj.displayArray(RCA.sum);
 
 	cout << "Delay :" << RCA.delayOfAdder << "D" << "\n";
 	cout << "C_32 : " << RCA.carryOut << "\n";
@@ -130,7 +122,8 @@ int main() {
 	free(a);
 	free(b); 
 	free(RCA.sum);
-}
 
+	return 0;
+}
 
 
